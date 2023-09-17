7051ae96834cbaa6834ea802eaddd4b07df3f0ca
Lukebalek <Lukebalek23@gmail.com>
2023-09-17 18:48:44 -0400

    Home Maintenance

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double summer = 1500;
+        double fall = 1390;
+        double winter = 2200;
+        double spring = 1850;
+        double totalCost = 0;
+
+        System.out.println("Your home maintenance for summer is " + summer);
+        System.out.println("Your home maintenance for fall is " + fall);
+        System.out.println("Your home maintenance for winter is "+ winter);
+        System.out.println("Your home maintenance for spring is " + spring);
+
+        totalCost = summer + fall + winter + spring;
+        System.out.println("The yearly home maintenance is " + totalCost);
     }
 }
